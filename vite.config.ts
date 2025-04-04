448fdab3b94c70a6b3b17c58d29cea726f69f259
Dhrutii <thakkardhruti22@gmail.com>
2025-04-04 13:05:31 +0530

    url changed

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://api.kidjig.com/provider",
+        target: "https://botmate.netlify.app",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
