46fc287e665e8b2d0e5aaf401544a2e2b21493d3
Dhrutii <thakkardhruti22@gmail.com>
2025-04-04 14:26:26 +0530

    redirect url

--- src/store/ChatStore.ts
+++ src/store/ChatStore.ts
@@ -1,10 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.DEV
-  ? "/api"
-  : "https://api.kidjig.com/provider";
-
 interface ChatState {
   input: string;
   isRequesting: boolean;
@@ -38,7 +34,7 @@ export const useChatStore = create<ChatState>((set) => ({
 
     try {
       const { data } = await axios.post(
-        `${BASE_URL}/v1/openai/chat/completions`,
+        `/api/v1/openai/chat/completions`,
         {
           model: "gpt-4o-mini",
           prompt: message,
